8f0df8100c42f70c8a5802e08fcf050f49355ebc
huynqcharles <huynq.charles@gmail.com>
2025-02-28 20:47:43 +0700

    update print info of dataset

--- data_processor.py
+++ data_processor.py
@@ -6,44 +6,35 @@ def prepare_data():
     spark = SparkSession.builder.getOrCreate()
 
     # Read data from HDFS
-    # SMALL
-    # dataset_path = "hdfs://localhost:9000/data/small/appendicitis_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/breast_cancer_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/breast_cancer_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/ecoli_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/ecoli_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/ionosphere_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/glass_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/monk-2_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/haberman_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/moons_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/ionosphere_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/newthyroid_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/iris_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/sonar_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/led7digit_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/spectfheart_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/monk-2_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/spiral_data.csv"
 
-    # dataset_path = "hdfs://localhost:9000/data/small/moons_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/movement_libras_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/newthyroid_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/sonar_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/spectfheart_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/spiral_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/vehicle_data.csv"
-
-    # dataset_path = "hdfs://localhost:9000/data/small/wine_data.csv"
-
-    dataset_path = "hdfs://localhost:9000/data/small/zoo_data.csv"
+    dataset_path = "hdfs://localhost:9000/data/small/vehicle_data.csv"
 
     df_data = spark.read.csv(dataset_path, header=True, inferSchema=True)
+
+    # Print dataset information
+    num_instances = df_data.count()
+    num_attributes = len(df_data.columns) - 1
+    num_classes = df_data.select('class').distinct().count()
+    print(f"Number of instances: {num_instances}")
+    print(f"Number of attributes: {num_attributes}")
+    print(f"Number of classes: {num_classes}")
 
     # Create id column
     df_data = df_data.withColumn("id", F.monotonically_increasing_id())
@@ -56,13 +47,7 @@ def prepare_data():
     df_data = assembler.transform(df_data).select("id", "features")
 
     # Read constraints from HDFS
-    # SMALL
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/appendicitis_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/appendicitis_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/appendicitis_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/appendicitis_constraints_0.2.json"
-    
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/breast_cancer_constraints_0.05.json"
+    constraints_path = "hdfs://localhost:9000/data/small/constraint sets/breast_cancer_constraints_0.05.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/breast_cancer_constraints_0.1.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/breast_cancer_constraints_0.15.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/breast_cancer_constraints_0.2.json"
@@ -72,30 +57,10 @@ def prepare_data():
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ecoli_constraints_0.15.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ecoli_constraints_0.2.json"
 
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/glass_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/glass_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/glass_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/glass_constraints_0.2.json"
-
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/haberman_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/haberman_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/haberman_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/haberman_constraints_0.2.json"
-
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ionosphere_constraints_0.05.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ionosphere_constraints_0.1.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ionosphere_constraints_0.15.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/ionosphere_constraints_0.2.json"
-
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/iris_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/iris_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/iris_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/iris_constraints_0.2.json"
-
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/led7digit_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/led7digit_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/led7digit_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/led7digit_constraints_0.2.json"
 
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/monk-2_constraints_0.05.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/monk-2_constraints_0.1.json"
@@ -106,11 +71,6 @@ def prepare_data():
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/moons_constraints_0.1.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/moons_constraints_0.15.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/moons_constraints_0.2.json"
-
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/movement_libras_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/movement_libras_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/movement_libras_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/movement_libras_constraints_0.2.json"
 
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/newthyroid_constraints_0.05.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/newthyroid_constraints_0.1.json"
@@ -137,16 +97,6 @@ def prepare_data():
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/vehicle_constraints_0.15.json"
     # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/vehicle_constraints_0.2.json"
 
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/wine_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/wine_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/wine_constraints_0.15.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/wine_constraints_0.2.json"
-
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/zoo_constraints_0.05.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/zoo_constraints_0.1.json"
-    # constraints_path = "hdfs://localhost:9000/data/small/constraint sets/zoo_constraints_0.15.json"
-    constraints_path = "hdfs://localhost:9000/data/small/constraint sets/zoo_constraints_0.2.json"
-
     df_json = spark.read.json(constraints_path)
 
     df_ml = df_json.selectExpr(
@@ -156,5 +106,7 @@ def prepare_data():
     df_cl = df_json.selectExpr(
         "inline(transform(cl, x -> struct(x[0] as id1, x[1] as id2)))"
     )
+
+
 
     return df_data, df_class, df_ml, df_cl
